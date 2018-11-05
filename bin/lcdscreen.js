359009b5fdd300d5fe91c417fe5629f3a056a8d5
nasser <nasser@sogilis.com>
2018-11-05 10:47:33 +0100

    validate with easy setup that is working end2end

--- bin/lcdscreen.js
+++ bin/lcdscreen.js
@@ -3,3 +3,23 @@
  *  entry point for the command line program
  * */ 
 
+var program = require('commander');
+let LCD = require("../lib/lcd");
+
+program
+    .usage("--show input number")
+    .option("-s, --show [inputnumber]", "number between 0-999 tp provide")
+    .parse(process.argv)
+
+/**
+ * program entry point
+ */
+function main() {
+    // Check params are valid
+
+    // Show 
+    let lcdscreen = new LCD(3,3)
+    lcdscreen.show();
+}
+
+main(); 

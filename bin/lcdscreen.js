97582d52d771f99a008bf1713a029e3432d00675
nasser <nasser@sogilis.com>
2018-11-05 12:47:04 +0100

    integrate cli with lcd class

--- bin/lcdscreen.js
+++ bin/lcdscreen.js
@@ -8,18 +8,35 @@ let LCD = require("../lib/lcd");
 
 program
     .usage("--show input number")
-    .option("-s, --show [inputnumber]", "number between 0-999 tp provide")
+    .option('-s, --show <inputnumber>', 'number between 0-999 tp provide')
     .parse(process.argv)
 
 /**
  * program entry point
  */
-function main() {
-    // Check params are valid
+function main(input) {
+    try {
+        //init
+        let lcdscreen = new LCD(3,3)
+        
+        // Check params
 
-    // Show 
-    let lcdscreen = new LCD(3,3)
-    lcdscreen.show();
+        //Show
+        if (lcdscreen.canShow(input)) {
+            //split input into an array
+            let digits = (""+input).split("");;
+            lcdscreen.show(digits);
+            process.exit(0);
+        } else {
+            console.log("input provided is not adapted for the screen, please use a number between 0 and 999")
+            process.exit(10);
+        }
+        
+    }
+    catch (errmsg) {
+        console.error(errmsg);
+        process.exit(20);
+    }
 }
 
-main(); 
+main(program.show); 
